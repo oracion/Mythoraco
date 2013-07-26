7758dc5a1476eee65d9d9be4d3603713290c9698
Jules Courtois <jules.courtoisetoile@laposte.net>
2013-07-26 22:25:27 +0200

    Updated buildings.php to include all the buildings we've thought of so far.
    Renamed each article class = "building" to article class = "building_*nameofbuilding*", for example article_class = "building_forum". Same for the buttons, but not for the asides.

--- buildings.php
+++ buildings.php
@@ -28,54 +28,135 @@ redir("index.php");
 <div id="mid_container">
 
 <section class="building_section">
-<article class="building">
-<p>Ce bâtiment est cool.</p>
-<p><input type="submit" value="Upgrade !" id="button" /></p>
+<article class="building_city">
+<p>The overall size and strength of your city.</p>
+<p><input type="submit" value="Upgrade !" id="button_city" /></p>
 <aside>
 </aside>
 </article>
 </section>
 
 <section class="building_section">
-<article class="building">
-<p>Ce bâtiment est fun.</p>
-<p><input type="submit" value="Upgrade !" id="button" /></p>
+<article class="building_air">
+<p>The elementary forge of air.</p>
+<p><input type="submit" value="Upgrade !" id="button_air" /></p>
 <aside>
 </aside>
 </article>
 </section>
 
 <section class="building_section">
-<article class="building">
-<p>Ce bâtiment est amusant.</p>
-<p><input type="submit" value="Upgrade !" id="button" /></p>
+<article class="building_earth">
+<p>The elementary forge of earth.</p>
+<p><input type="submit" value="Upgrade !" id="button_earth" /></p>
 <aside>
 </aside>
 </article>
 </section>
 
 <section class="building_section">
-<article class="building">
-<p>Ce bâtiment est puissant.</p>
-<p><input type="submit" value="Upgrade !" id="button" /></p>
+<article class="building_fire">
+<p>The elementary forge of fire.</p>
+<p><input type="submit" value="Upgrade !" id="button_fire" /></p>
 <aside>
 </aside>
 </article>
 </section>
 
 <section class="building_section">
-<article class="building">
-<p>Ce bâtiment est op.</p>
-<p><input type="submit" value="Upgrade !" id="button" /></p>
+<article class="building_water">
+<p>The elementary forge of water.</p>
+<p><input type="submit" value="Upgrade !" id="button_water" /></p>
 <aside>
 </aside>
 </article>
 </section>
 
 <section class="building_section">
-<article class="building">
-<p>Ce bâtiment ne sert à rien, mais il coûte cher.</p>
-<p><input type="submit" value="Upgrade !" id="button" /></p>
+<article class="building_wood">
+<p>Where your heroes collect wood.</p>
+<p><input type="submit" value="Upgrade !" id="button_wood" /></p>
+<aside>
+</aside>
+</article>
+</section>
+
+<section class="building_section">
+<article class="building_stone">
+<p>Where your heroes collect stone.</p>
+<p><input type="submit" value="Upgrade !" id="button_stone" /></p>
+<aside>
+</aside>
+</article>
+</section>
+
+<section class="building_section">
+<article class="building_iron">
+<p>Where your heroes collect iron.</p>
+<p><input type="submit" value="Upgrade !" id="button_iron" /></p>
+<aside>
+</aside>
+</article>
+</section>
+
+<section class="building_section">
+<article class="building_quartz">
+<p>Where your heroes collect quartz.</p>
+<p><input type="submit" value="Upgrade !" id="button_quartz" /></p>
+<aside>
+</aside>
+</article>
+</section>
+
+<section class="building_section">
+<article class="building_armory">
+<p>This is the armory, where you summon heros, create and repair equipment.</p>
+<p><input type="submit" value="Upgrade !" id="button_armory" /></p>
+<aside>
+</aside>
+</article>
+</section>
+
+<section class="building_section">
+<article class="building_houses">
+<p>This is where your heroes live and rest.</p>
+<p><input type="submit" value="Upgrade !" id="button_houses" /></p>
+<aside>
+</aside>
+</article>
+</section>
+
+<section class="building_section">
+<article class="building_temple">
+<p>This is where your heroes pray to the gods and gain faith.</p>
+<p><input type="submit" value="Upgrade !" id="button_temple" /></p>
+<aside>
+</aside>
+</article>
+</section>
+
+<section class="building_section">
+<article class="building_baths">
+<p>This is where your citizens relax and gain happiness.</p>
+<p><input type="submit" value="Upgrade !" id="button_baths" /></p>
+<aside>
+</aside>
+</article>
+</section>
+
+<section class="building_section">
+<article class="building_market">
+<p>This is zhere your citizens exchange goods and gold.</p>
+<p><input type="submit" value="Upgrade !" id="button_market" /></p>
+<aside>
+</aside>
+</article>
+</section>
+
+<section class="building_section">
+<article class="building_forum">
+<p>This building is where ambassadors from other cities gather to make pacts and treaties.</p>
+<p><input type="submit" value="Upgrade !" id="button_forum" /></p>
 <aside>
 </aside>
 </article>
