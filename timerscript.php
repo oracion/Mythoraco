0099dc7c3c9993fa6d68ae9abcd567ddb52863a6
Jules Courtois <jules.courtoisetoile@laposte.net>
2013-08-02 09:36:02 -1000

    Completion de timerscript.php
    Normalement, plus besoin de toucher a ce script ; il est fini.

--- timerscript.php
+++ timerscript.php
@@ -5,7 +5,7 @@ echo "<script language=\"javascript\">";
 echo "window.location='$url';"; 
 echo "</script>";  }
 
-//On commence la connection à la database//
+//On commence la connection ï¿½ la database//
 try 
 {
 $bdd = new PDO('mysql:host=localhost;dbname=mythoracodb', 'root', '');
@@ -23,19 +23,30 @@ while ($donneesbuildings = $reqreadbuildings->fetch())
 $currentid = $donneesbuildings['userid'];
 while ($donneesressources = $reqreadressources->fetch())
 {
-$newstone = $donneesressources['stone']+($donneesbuildings['levelquarry']*$donneesbuildings['levelquarry']*0.25 + $donneesbuildings['levelquarry']*10);
-$newwood = $donneesressources['wood']+($donneesbuildings['levelsawmill']*$donneesbuildings['levelsawmill']*0.25 + $donneesbuildings['levelsawmill']*10);
-$newiron = $donneesressources['iron']+($donneesbuildings['leveliron']*$donneesbuildings['leveliron']*0.25 + $donneesbuildings['leveliron']*10);
-$newquartz = $donneesressources['quartz']+($donneesbuildings['levelquartz']*$donneesbuildings['levelquartz']*0.25 + $donneesbuildings['levelquartz']*10);
-$reqwrite = $bdd->prepare('UPDATE ressources SET 
-stone = :stone, wood = :wood, iron = :iron, quartz = :quartz 
+$newair    = $donneesressources['air']    + ($donneesbuildings['levelair']*$donneesbuildings['levelair']*0.5 + $donneesbuildings['levelair']*3)               / 360;
+$newearth  = $donneesressources['earth']  + ($donneesbuildings['levelearth']*$donneesbuildings['levelearth']*0.5 + $donneesbuildings['levelearth']*3)         / 360;
+$newfire   = $donneesressources['fire']   + ($donneesbuildings['levelfire']*$donneesbuildings['levelfire']*0.5 + $donneesbuildings['levelfire']*3)            / 360;
+$newwater  = $donneesressources['water']  + ($donneesbuildings['levelwater']*$donneesbuildings['levelwater']*0.5 + $donneesbuildings['levelwater']*3)         / 360;
+$newstone  = $donneesressources['stone']  + ($donneesbuildings['levelquarry']*$donneesbuildings['levelquarry']*0.25 + $donneesbuildings['levelquarry']*10)    / 360;
+$newwood   = $donneesressources['wood']   + ($donneesbuildings['levelsawmill']*$donneesbuildings['levelsawmill']*0.25 + $donneesbuildings['levelsawmill']*10) / 360;
+$newiron   = $donneesressources['iron']   + ($donneesbuildings['leveliron']*$donneesbuildings['leveliron']*0.25 + $donneesbuildings['leveliron']*10)          / 360;
+$newquartz = $donneesressources['quartz'] + ($donneesbuildings['levelquartz']*$donneesbuildings['levelquartz']*0.25 + $donneesbuildings['levelquartz']*10)    / 360;
+$newgold   = $donneesressources['gold']   + (($donneesressources['happiness']/100) * $donneesbuildings['houses']) / 360;
+$reqwrite = $bdd->prepare('
+UPDATE ressources 
+SET air = :air, earth = :earth, fire = :fire, water = :water, stone = :stone, wood = :wood, iron = :iron, quartz = :quartz, gold = :gold
 WHERE id = :id');
 $reqwrite->execute(array(
- 'stone' => $newstone,
- 'wood' => $newwood,
- 'iron' => $newiron,
- 'quartz' => $newquartz,
- 'id' => $currentid
+ 'air'     => $newair,
+ 'earth'   => $newearth,
+ 'fire'    => $newfire,
+ 'water'   => $newwater,
+ 'stone'   => $newstone,
+ 'wood'    => $newwood,
+ 'iron'    => $newiron,
+ 'quartz'  => $newquartz,
+ 'gold'    => $newgold,
+ 'id'      => $currentid
  ));
 
 }
