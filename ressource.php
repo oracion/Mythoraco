d1a0f8499b712623c0e36f92a5bec75b05542203
Jules Courtois <jules.courtoisetoile@laposte.net>
2013-08-19 07:39:40 +0200

    Utilise desormais le bon nombre de ressources.

--- ressource.php
+++ ressource.php
@@ -1,8 +1,27 @@
+<?php
+try
+{
+$bdd = new PDO('mysql:host=localhost;dbname=mythoracodb', 'root', '');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+$req = $bdd->prepare('SELECT * FROM ressources WHERE userid = ?');
+$req->execute(array($_SESSION['id']));
+while ($donnees = $req->fetch())
+{
+?>
+
 <div id="ressource">
        <ul>
            <li><img src="terre.png" alt="Element terre" title="Terre"><b>Terre</b></li>
+           <?php echo $donnees['earth']; ?>
            <li><img src="air.png" alt="Element air" title="Air"><b>Air</b></li>
+           <?php echo $donnees['air']; ?>
            <li><img src="feu.png" alt="Element feu" title="Feu"><b>Feu</b></li>
-		   <li><img src="eau.png" alt="Element eau" title="Eau"><b>Eau</b></li>
+           <?php echo $donnees['fire']; ?>
+	   <li><img src="eau.png" alt="Element eau" title="Eau"><b>Eau</b></li>
+           <?php echo $donnees['water']; ?>
        </ul>
 </div>
