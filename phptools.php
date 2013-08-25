203aaebe4e5193a55c8bf301f9fcc677f7bceefd
Jules Courtois <jules.courtoisetoile@laposte.net>
2013-08-25 05:22:29 +0200

    Added redir function

--- phptools.php
+++ phptools.php
@@ -1,2 +1,14 @@
 <?php
+//This is a file to include in other pages to reuse frequent functions.
+
+//The redir function to redirect to another page.
+function redir($url)
+{
+  echo "<script language=\"javascript\">";
+  echo "window.location='$url';";
+  echo "</script>"; 
+}
+
+//The function to read all values for a given player id in the 'ressources' database.
+
 ?>
