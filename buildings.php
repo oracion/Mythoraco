df0fce6a4ac95383d6efcd841dca687d06bbd229
Jules Courtois <jules.courtoisetoile@laposte.net>
2013-08-25 05:26:30 +0200

    Removed redir function

--- buildings.php
+++ buildings.php
@@ -1,11 +1,9 @@
 <?php
+include("phptools.php")
+?>
+
+<?php
 session_start();
-// la fonction de redirection ------------ 
-function redir($url){ 
-echo "<script language=\"javascript\">"; 
-echo "window.location='$url';"; 
-echo "</script>"; 
-} 
 if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] = false)
 {
 redir("index.php");
