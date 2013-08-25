58cc35e2f50b3ecb828c105cd1c0c7b210c9e749
Jules Courtois <jules.courtoisetoile@laposte.net>
2013-08-25 05:27:45 +0200

    Added redir function

--- disconnect.php
+++ disconnect.php
@@ -1,16 +1,14 @@
 <?php
+include("phptools.php")
+?>
+
+<?php
 session_start();
 
 if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] = false)
 {
 redir("index.php");
 }
-
-// la fonction de redirection ------------ 
-function redir($url){ 
-echo "<script language=\"javascript\">"; 
-echo "window.location='$url';"; 
-echo "</script>";  }
 
 $_SESSION['logged_in'] = false;
 redir("index.php"); 
